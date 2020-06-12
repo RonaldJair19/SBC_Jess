3fc5919454120d6e5fc5170ebb6400a30209d878
Ronald Juarez <jairudas1998@gmail.com>
2020-06-11 23:06:10 -0500

    Se crea la clase MotorControlador

--- src/sbc_mecanico/ControlMotor.java
+++ src/sbc_mecanico/ControlMotor.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sbc_mecanico;
 import jess.JessException;
 import jess.Rete;
@@ -10,9 +5,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import jess.*;
 
-
-public class SBC_Mecanico {
-
+public class ControlMotor {
     public static String ruta = "E:\\Documentos\\UTP\\Cuarto_Anio\\Sistemas_basados_en_el_conocimiento\\SBC_Mecanico\\SE_Mecanico.clp";
     public static String hecho = "";
     
@@ -25,13 +18,5 @@ public class SBC_Mecanico {
         }catch (JessException ex){
             Logger.getLogger(SBC_Mecanico.class.getName()).log(Level.SEVERE, null,ex);
         }
-        
-        
-        
-
     }
-    
-    
-    
-    
 }
