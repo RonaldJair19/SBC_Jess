7d43a904f2a489c131e94939773e681c3f05b622
Ronald Juarez <jairudas1998@gmail.com>
2020-06-13 16:40:47 -0500

    Se cambio a Jframe, no se ha probado si sirve

--- src/sbc_mecanico/SBC_Mecanico.java
+++ src/sbc_mecanico/SBC_Mecanico.java
@@ -1,11 +1,13 @@
 package sbc_mecanico;
-
-
+import Interfaz.Interfaz_SBC;
 
 public class SBC_Mecanico {
-    public static void main(String args[]){
+    public static void main(String[] args){
+      Interfaz_SBC InterfazPrincipal = new Interfaz_SBC();
       ControlMotor controlMotor = new ControlMotor();
+      
+      InterfazPrincipal.setControlMotor(controlMotor);
+      
       controlMotor.F_Ejecutar();
-    }
-    
+     }
     }
